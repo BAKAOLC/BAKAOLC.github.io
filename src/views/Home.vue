3d1281daca8f60dfca832ed402f18ec45736180d
OLC <ritsukage00@gmail.com>
2025-09-04 10:04:53 +0800

    删除调试面板

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="home-page">
     <personal-section />
-    <screen-manager-test />
   </div>
 </template>
 
 <script setup lang="ts">
 import PersonalSection from '@/components/PersonalSection.vue';
-import ScreenManagerTest from '@/components/ScreenManagerTest.vue';
 </script>
 
 <style scoped>
